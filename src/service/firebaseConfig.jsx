202bb9082251a00d09c5268289c6218ba5ee6e69
Nidhi Rawat <168139652+nidhir08@users.noreply.github.com>
2025-05-27 04:15:32 +0530

    updates

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmSJ4KXtGg8ZPAJawjbr0NBVY3pX2oYe4",
-  authDomain: "quiet-result-454211-a5.firebaseapp.com",
-  projectId: "quiet-result-454211-a5",
-  storageBucket: "quiet-result-454211-a5.firebasestorage.app",
-  messagingSenderId: "609008795223",
-  appId: "1:609008795223:web:a2cb042dd528f3a2e62d12",
-  measurementId: "G-WGB93G2DWX"
+  apiKey: import.meta.env.FIREBASE_APIKEY,
+  authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.FFIREBASE_MESSAGING,
+  appId: import.meta.env.FIREBASE_APPID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT
 };
 
 // Initialize Firebase
